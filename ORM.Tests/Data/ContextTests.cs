59876e27b79a45b6ff10e0424f29d72862f86051
Ipsit Gaur <ipsit.gaur@bold.com>
2018-12-20 16:47:05 +0530

    Adding unit tests

--- ORM.Tests/Data/ContextTests.cs
+++ ORM.Tests/Data/ContextTests.cs
@@ -1,35 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace ORM.Tests.Data
 {
     [TestClass]
     public class ContextTests
     {
+        private TestContext _testContext;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _testContext = new TestContext();
+        }
+
         [TestMethod]
         public void ContextCreationTest()
         {
-            TestContext myContext = new TestContext();
+            Assert.IsNotNull(_testContext);
         }
 
         [TestMethod]
-        public void DbTableFilterTest()
+        public void ContextDbSetsInitializedTest()
         {
-            var myContext = new TestContext();
-            var testID = 100000;
-            var error = myContext.Errors.Filter(x => x.ErrorID == 100000).Read();
-            Assert.AreEqual(1, error.Count);
-            Assert.AreEqual(error.FirstOrDefault().ErrorID, testID);
-        }
-
-        [TestMethod]
-        public void DbTableMultilpleFilterTest()
-        {
-            var myContext = new TestContext();
-            var error = myContext.Errors
-                .Filter(x => x.ErrorID == 100000)
-                .Filter(y => y.Description != "abc").Read();
-            Assert.AreEqual(1, error.Count);
+            Assert.IsNotNull(_testContext.Errors);
         }
     }
 }
---
+++ ORM.Tests/Data/DbTableTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ORM.Tests.Data
+{
+    [TestClass]
+    public class DbTableTests
+    {
+        private TestContext _testContext;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _testContext = new TestContext();
+        }
+
+        [TestMethod]
+        public void DbTableFilterTest()
+        {
+            var testID = 100000;
+            var error = _testContext.Errors.Filter(x => x.ErrorID == 100000).Read();
+            Assert.AreEqual(1, error.Count);
+            Assert.AreEqual(error.FirstOrDefault().ErrorID, testID);
+        }
+
+        [TestMethod]
+        public void DbTableMultilpleFilterTest()
+        {
+            var error = _testContext.Errors
+                .Filter(x => x.ErrorID == 100000)
+                .Filter(y => y.Description != "abc").Read();
+            Assert.AreEqual(1, error.Count);
+        }
+    }
+}
