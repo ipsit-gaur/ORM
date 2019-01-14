56052ce9eb8e572e53c1013ab795258398901f2f
Ipsit Gaur <ipsit.gaur@bold.com>
2019-01-14 12:20:01 +0530

    Added added attribute for Primary key

--- ORM.Tests/TestTable.cs
+++ ORM.Tests/TestTable.cs
@@ -1,9 +1,11 @@
-﻿using ORM.Data;
+﻿using ORM.Attributes;
+using ORM.Data;
 
 namespace ORM.Tests
 {
     public class TestTable : DbEntity
     {
+        [Key]
         public int ID { get; set; }
         public string Text { get; set; }
     }
